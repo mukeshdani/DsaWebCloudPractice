322ca6a0532f61ce5169d3be0ae3afdbf6b1a0e3
mukeshdani <mukeshdani00@gmail.com>
2025-05-21 23:43:34 +0530

    CircularQueue is implemented using java

--- JAVA/Queue/CircularQueue/CircularQueue.java
+++ JAVA/Queue/CircularQueue/CircularQueue.java
@@ -13,6 +13,56 @@ public class CircularQueue {
         this.rear = -1;
         this.size = 0;
     }
-    
+    public boolean isQueueFull(){
+        if(size == capacity){
+            System.out.println("Queue is full");
+            return true;
+        }else {
+            return false;
+        }
+    }
+    public boolean isQueueEmpty(){
+        if(size==0){
+            System.out.println("Queue is Empty!!!");
+            return true;
+        }
+        return false;
+    }
+    //Insert an element :[R]
+    public void enqueue(int data){
+        if(isQueueFull()){
+            return;
+        }
+        //capacity = 10;
+        //rear (9+1)%10 === 0;
+        rear = (rear+1)%capacity;
+        queue[rear] = data;
+        size++;
+    }
+
+    //Delete 
+    public int dequeue(){
+        if(isQueueEmpty()){
+            return -1;
+        }
+        int value = queue[front];
+        front = (front+1)%capacity;
+        size--;
+        return value;
+    }
+
+    public int size(){
+        return this.size;
+    }
+
+    public void display(){
+        if(isQueueEmpty()){
+            return;
+        }
+        for(int i = 0; i<=size ; i++){
+            System.out.print(queue[(front+i)%capacity] + " " + '\t');
+        }
+        System.out.println();
+    }
     
 }
--- JAVA/Queue/CircularQueue/main.java
+++ JAVA/Queue/CircularQueue/main.java
@@ -2,6 +2,25 @@ package JAVA.Queue.CircularQueue;
 
 public class main {
     public static void main(String[] args) {
-        
+        CircularQueue circularQueue = new CircularQueue(5);
+        circularQueue.enqueue(10);
+        circularQueue.enqueue(80);
+        circularQueue.enqueue(40);
+        circularQueue.enqueue(10);
+        circularQueue.enqueue(80);
+        circularQueue.enqueue(40);
+        System.err.println("Size of circular queue is: " + circularQueue.size());
+        circularQueue.display();
+        circularQueue.dequeue();
+        circularQueue.dequeue();
+        circularQueue.display();
+        System.err.println("Size of circular queue is: " + circularQueue.size());
+        circularQueue.enqueue(10);
+        circularQueue.enqueue(80);
+        circularQueue.enqueue(40);
+        circularQueue.display();
+        System.err.println("Size of circular queue is: " + circularQueue.size());
+        circularQueue.display();
+        circularQueue.display();
     }
 }
