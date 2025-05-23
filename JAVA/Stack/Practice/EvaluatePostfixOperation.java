1b88bdf11e5383bcf1afa5bd25addf0a4f190e2f
mukeshdani <mukeshdani00@gmail.com>
2025-05-23 14:51:39 +0530

    Changes

--- JAVA/Stack/Practice/EvaluatePostfixOperation.java
+++ JAVA/Stack/Practice/EvaluatePostfixOperation.java
@@ -1,5 +1,20 @@
 package JAVA.Stack.Practice;
 
+import java.util.Stack;
+
 public class EvaluatePostfixOperation {
+
+    public static void main(String[] args) {
+        String str = "3+2+2";//322++
+        Stack stack = new Stack<>();
+
+        for(int i = 0 ; i < str.length() ; i++){
+            char ch = str.charAt(i);
+            
+            if()
+        }
+
+
+    }
     
 }
