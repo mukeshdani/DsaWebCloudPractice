02dd7a2cbdd8e9b31761fd9ad3358988ee48c79a
mukeshdani <mukeshdani00@gmail.com>
2025-06-14 19:16:48 +0530

    useEffect Hooks fixs

---
+++ REACT/Vite/react-blog/src/Hooks/Hook.jsx
@@ -0,0 +1,22 @@
+// Hooks In react
+
+// What are hooks
+// Why we need hooks
+// History of hooks
+// Some Hooks name
+// How to identify Hooks
+
+
+// What are hooks (after 16.2)
+// In old React version, we were using class base component
+// now class based component are not much using in React
+// So to achieve State, life cycle, other features in functional component we use hooks
+
+// Popular React Hooks
+// useState
+// useEffect
+// useContext
+// useRef
+// useReducer
+// etc.
+
---
+++ REACT/Vite/react-blog/src/Hooks/useEffect/useEffect.jsx
@@ -0,0 +1,26 @@
+function UseEffect(){
+    return <div>
+
+    </div>
+}
+
+export default UseEffect
+
+// useEffect Hooks
+// Remove side effect inside component (state)
+// Remove side effect from outside cmponent (props)
+// Use to fetch data
+// Can use as life cycle methods 
+// Can use for DOM manipulation
+// Much more
+
+// Basic use of useEffect
+// Prevent Extra rendering of comonent on state
+// Very important interview
+
+
+// Syntax of useEffect
+//  useEffect(pass function as parameter, dependency)
+//  useEffect(()=>{
+//   // write your code
+//   }, [state or props])
