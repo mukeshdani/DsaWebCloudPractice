d00ea67efd4d78becb2f39da56f318d058af5972
mukeshdani <mukeshdani00@gmail.com>
2025-06-14 10:53:35 +0530

    Controlled Component

--- REACT/Vite/react-blog/src/ControlledComp.jsx
+++ REACT/Vite/react-blog/src/ControlledComp.jsx
@@ -1,6 +1,47 @@
+import { useState } from "react"
+
 function ControlledComp(){
+
+    const [name, setName] = useState("")
+    const [pass, setPass] = useState("")
+    const [email, setEmail] = useState("")
+
     return <div>
         <h1>Controlled Componnet</h1>
+        <form action="" method="get">
+            <input type="text" value ={name} onChange={(event)=>{
+                setName(event.target.value)
+            }} placeholder= "Enter Name"></input>
+
+            <br></br>
+            
+            <input type="Password" value ={pass} onChange={(event)=>{
+                setPass(event.target.value)
+            }} placeholder= "Enter Password"></input>
+            
+            <br></br>
+            
+            <input type="Email" value ={email} onChange = {(event)=>{
+                setEmail(event.target.value)
+            }} placeholder= "Enter Email"></input>
+            
+            <br></br>
+            
+            <br></br>
+            
+            <button >Submit</button>
+            
+            <br></br>
+            <h3>{name}</h3>
+            <h3>{pass}</h3>
+            <h3>{email}</h3>
+            
+            <br></br>
+            
+            <button onClick={()=>{
+                setEmail(""); setName(''); setPass('')
+            }}>Clear</button>
+        </form>
     </div>
 }
 
@@ -13,3 +54,9 @@ export default ControlledComp
 // Store input field value in state
 // Use Change handler with input field
 // Value attribute attached with state
+
+// Benefits
+// Single source of truth 
+// Validation and Manipulation Before submit
+// Dynamuc Update Values
+
