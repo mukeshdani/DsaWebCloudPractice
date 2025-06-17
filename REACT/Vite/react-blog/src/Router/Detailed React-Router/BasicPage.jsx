ad57c7cd3a99067fb5e45574381f274dd4ce5459
mukeshdani <mukeshdani00@gmail.com>
2025-06-17 12:45:35 +0530

    Route Prefix

--- REACT/Vite/react-blog/src/Router/Detailed React-Router/BasicPage.jsx
+++ REACT/Vite/react-blog/src/Router/Detailed React-Router/BasicPage.jsx
@@ -17,8 +17,11 @@ function BasicPage() {
       <Routes>
         <Route element={<NavBar />}>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/user">
+               <Route path="/user/login" element={<Login />} />
+               <Route path="/user/about" element={<About />} />
+          </Route>
+          
         </Route>
 
 
@@ -52,3 +55,21 @@ export default BasicPage
 // Link: A link for navigate from 1 page to other page
 // 
 
+// Outlet:Outlet: Multi-Level Nested Routing
+// The <Outlet /> component is used in a parent route to render child routes. It acts as a placeholder.
+
+// Multi Navigation Nested Routing
+// Layout and Index Routes: Layout and Index Routes
+
+// Layout Route:
+// A layout route wraps multiple routes with a common layout (like a navbar or sidebar).
+
+// Index Route:
+// An index route is the default child route rendered when the parent path is matched.
+
+// Route prefix: 3. Route Prefix
+// React Router automatically handles route prefixes in nested routes. You don’t need to write /college/student, just use student inside the nested route.
+
+// 4. Page Not Found Route
+// To handle undefined routes, use a wildcard (*) path.
+// // Page Not Found Page
--- REACT/Vite/react-blog/src/Router/Detailed React-Router/NavBar.jsx
+++ REACT/Vite/react-blog/src/Router/Detailed React-Router/NavBar.jsx
@@ -13,10 +13,10 @@ export default function NavBar(){
                     <Link className="link" to='/'>Home</Link>
                 </li>
                 <li>
-                    <Link className="link" to='/about'>About</Link>
+                    <Link className="link" to='/user/about'>About</Link>
                 </li>
                 <li>
-                    <Link className="link" to='/login'>Login</Link>
+                    <Link className="link" to='/user/login'>Login</Link>
                 </li>
                 <li>
                     <Link className="link" to='/college'>College</Link>
