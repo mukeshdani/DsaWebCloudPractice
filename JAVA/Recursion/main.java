871b1fe38ed5c7e0df450117d8dbfbbb39a6594a
mukeshdani <mukeshdani00@gmail.com>
2025-06-26 14:36:51 +0530

    fix

---
+++ JAVA/Recursion/main.java
@@ -0,0 +1,13 @@
+
+
+public class main{
+
+    public static void message(){
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] args) {
+        message();
+    }
+
+}
