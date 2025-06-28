9d04d5c40edd7b6959249b100dbef22db5fb82f3
mukeshdani <mukeshdani00@gmail.com>
2025-06-28 19:29:26 +0530

    Fix Docker and k8 connection error fix

--- JAVASCRIPT/Auto-Retry Promise/index.js
+++ JAVASCRIPT/Auto-Retry Promise/index.js
@@ -1,31 +1,32 @@
 let attemptCounter = 0;
 
 const fetchData = async (APIURL, retries, delay) => {
-  try {
-    
-    attemptCounter++;
-    if(attemptCounter <= 3) {
-      throw new Error("Simulated network failure")
-    }
+    try {
 
-    const response = await fetch(APIURL);
-    console.log("Success:", response.status)
-    return response.data;
-  } catch(err) {
-    console.log(`Attempt ${attemptCounter} failed with error ${err.message}`)
-    if(retries > 0) {
-      // setTimeout(() => {
-      // }, delay)
+        attemptCounter++;
+        if (attemptCounter <= 3) {
+            throw new Error("Simulated network failure")
+        }
 
-      await new Promise(resolve => setTimeout(resolve,delay))
-      fetchData(APIURL, retries - 1, delay)
-    } else {
-      throw new Error("All retries failed");
+        const response = await fetch(APIURL);
+        console.log("Success:", response.status)
+        return response.data;
+    } catch (err) {
+        console.log(`Attempt ${attemptCounter} failed with error ${err.message}`)
+        if (retries > 0) {
+            // setTimeout(() => {
+            // }, delay)
+
+            await new Promise(resolve => setTimeout(resolve, delay))
+            fetchData(APIURL, retries - 1, delay)
+        } else {
+            throw new Error("All retries failed");
+        }
     }
-  }
 
 }
 
 const APIURL = 'https://jsonplaceholder.typicode.com/posts/1'
 
 fetchData(APIURL, 5, 1000);
+
