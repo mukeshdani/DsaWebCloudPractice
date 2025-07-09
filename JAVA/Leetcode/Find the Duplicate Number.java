e9881e55a61746fdb4ba1d18608216e5e3492baa
mukeshdani <mukeshdani00@gmail.com>
2025-07-09 18:16:51 +0530

    java code solutions

---
+++ JAVA/Leetcode/Find the Duplicate Number.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int [] ans = new int[2];
+        int i = 0 ;
+          while(i < nums.length){
+                int correctIndex=nums[i] - 1;
+                if( nums[i] != nums[correctIndex]){
+                     int temp = nums[i];
+                     nums[i] = nums[correctIndex];
+                     nums[correctIndex] = temp;
+                }else {
+                    i++;
+                }
+            }
+
+        for(int j = 0 ; j< nums.length ;j++){
+            if(nums[j] != j+1){
+                ans[0] = nums[j];
+                ans[1] = j+1;
+            }
+        }
+
+        return ans;
+    }
+}
