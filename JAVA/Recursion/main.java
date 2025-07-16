4d6f7803de9a4e8ab18538e1a23109b9b73470fd
mukeshdani <mukeshdani00@gmail.com>
2025-07-16 10:19:56 +0530

    revise

--- JAVA/Recursion/main.java
+++ JAVA/Recursion/main.java
@@ -8,7 +8,25 @@ public class main{
 
     public static void main(String[] args) {
         // message();
-        System.out.println(fibo2(4));
+        // System.out.println(fibo2(4));
+        int [] arr = {1,2,3,47,89,90,98,99,1002,30002};
+        int target = 1002;
+        System.out.println(binarySearch(arr, target, 0 , arr.length - 1));
+        
+    }
+
+    static int binarySearch(int[] arr, int target, int s, int e){
+        if(s>e){
+            return -1;
+        }
+        int m = s + (e-s)/2;
+        if(arr[m] == target){
+            return m;
+        }else if(target < arr[m]){
+            return binarySearch(arr , target, s , m - 1);
+        }else {
+            return binarySearch(arr , target,  m + 1, e);
+        }
     }
 
     static int fibo(int n ){
